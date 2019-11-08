f91bfc05c0772192131b41cc4ff9e96259d6e36d
Hiroyuki Shirakawa <hiroyuki.shirakawa@viibar.com>
2019-11-08 19:24:40 +0900

    send unicode

--- keyboards/meishi2/keymaps/shrkw/keymap.c
+++ keyboards/meishi2/keymaps/shrkw/keymap.c
@@ -15,9 +15,39 @@
  */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  MCR1,
+  MCR2,
+  MCR3,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MCR1:
+      if (record->event.pressed) {
+      }
+      return false;
+      break;
+    case MCR2:
+      if (record->event.pressed) {
+        send_unicode_hex_string("0035 0030 0030 0030 5146 5186 6B32 3057 3044 0021");  // 5000兆円欲しい!
+      }
+      return false;
+      break;
+    case MCR3:
+      if (record->event.pressed) {
+        SEND_STRING("hellohello");
+      }
+      return false;
+      break;
+  }
+  return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    LCTL(KC_SPACE),  KC_SPACE,  LGUI(LALT(KC_LEFT)), LGUI(LALT(KC_RIGHT)) \
+    MCR1,  MCR2,  LGUI(LALT(KC_LEFT)), LGUI(LALT(KC_RIGHT)) \
   )
 };
 
