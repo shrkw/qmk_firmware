257f18fe38b2b9632fff9611754c97d5bd8689fd
Hiroyuki Shirakawa <hiroyuki.shirakawa@viibar.com>
2019-12-09 15:35:49 +0900

    LT conflicts to update_tri_layer

--- keyboards/lily58/keymaps/shrkw/keymap.c
+++ keyboards/lily58/keymaps/shrkw/keymap.c
@@ -35,8 +35,8 @@ enum custom_keycodes {
 #define FN_LEFT LGUI(LALT(KC_LEFT))
 #define FN_RGHT LGUI(LALT(KC_RIGHT))
 #define FN_IME LCTL(KC_SPC)
-#define FN_LO_2 LT(_LOWER, KC_LANG2)
-#define FN_RA_1 LT(_RAISE, KC_LANG1)
+#define FN_GUI2 GUI_T(KC_LANG2)
+#define FN_GUI1 GUI_T(KC_LANG1)
 #define FN_SCRL LCTL(LSFT(KC_POWER))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |LANG2 |/       /         \      \ |LANG1 |      |      |
+ *                   |      |LANG2 |      |/       /         \      \ |      |      |LANG1 |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_LALT, KC_LGUI,FN_LO_2, KC_SPC, KC_ENT,   FN_RA_1,   KC_BSPC, KC_RGUI \
+                             KC_LALT, FN_GUI2, LOWER,KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, FN_GUI1 \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
